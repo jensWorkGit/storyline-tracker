6f5ea1988307fc583e119e98b90cc7a8da6be241
John Papa <john@johnpapa.net>
2016-01-12 23:53:43 -0500

    msg for modal

--- app/blocks/modal/modal.service.ts
+++ app/blocks/modal/modal.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from 'angular2/core';
 @Injectable()
 export class ModalService {
   title: string = 'Confirmation';
-  message: string = 'Do you want to cancel?';
+  message: string = 'Do you want to cancel your changes?';
   cancelText: string = 'Cancel';
   okText: string = 'OK';
   activate: () => Promise<boolean>;
